16090cba09071d408209f5717f54070b87ab2ab1
Fidel Salgado <fidels@stanford.edu>
2016-09-09 01:38:40 -0700

    completed step 4 and refactored the steps

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ var request = require("request"),
     code2040Token = '435537725d12267f4efa19879e506442',
     githubUrl = 'https://github.com/fidelsalgado/code2040Application/';
 
-var defaultOptions = { 
-  method: 'POST',
-  headers: { 'content-type': 'multipart/form-data;' },
-  formData: { token: code2040Token, github: githubUrl }
-};
+function postOptions(url, data) {
+  var options = { url: url, method: 'POST', json: data }
+  options.json.token = code2040Token
+  return options;
+}
 
 function step1() {
-  defaultOptions.url = code2040Url + 'register';
-  request(defaultOptions, function (error, response, body) {
+  var url = code2040Url + 'register';
+  var data = {github: githubUrl};
+  request(postOptions(url, data), function (error, response, body) {
     if (error) throw new Error(error);
     console.log(body);
   });
@@ -19,14 +20,13 @@ function step1() {
 
 function step2() {
   /* Get the string to reverse */
-  defaultOptions.url = code2040Url + 'reverse';
-  request(defaultOptions, function (error, response, body) {
+  var url = code2040Url + 'reverse';
+  request(postOptions(url, {}), function (error, response, body) {
     if (error) throw new Error(error);
     
-    /* Reverse string and send it */
-    defaultOptions.url = code2040Url + 'reverse/validate';
-    defaultOptions.formData.string = body.split("").reverse().join("");
-    request(defaultOptions, function (error, response, body) {
+    var url = code2040Url + 'reverse/validate';
+    var data = {string: body.split("").reverse().join("")}
+    request(postOptions(url, data), function (error, response, body) {
       if (error) throw new Error(error);
       console.log(body);
     });
@@ -34,15 +34,13 @@ function step2() {
 }
 
 function step3() {
-  defaultOptions.url = code2040Url + 'haystack';
-  request(defaultOptions, function (error, response, body) {
+  var url = code2040Url + 'haystack';
+  request(postOptions(url, {}), function (error, response, body) {
     if (error) throw new Error(error);
     
-    /* Find needle index and send it*/
-    defaultOptions.url = code2040Url + 'haystack/validate';
-    body = JSON.parse(body);
-    defaultOptions.formData.needle = body.haystack.indexOf(body.needle);
-    request(defaultOptions, function (error, response, body) {
+    var url = code2040Url + 'haystack/validate';
+    var data = {needle: body.haystack.indexOf(body.needle)};
+    request(postOptions(url, data), function (error, response, body) {
       if (error) throw new Error(error);
       console.log(body);
     });
@@ -50,20 +48,17 @@ function step3() {
 }
 
 function step4() {
-  defaultOptions.url = code2040Url + 'prefix';
-  request(defaultOptions, function (error, response, body) {
+  var url = code2040Url + 'prefix';
+  request(postOptions(url, {}), function (error, response, body) {
     if (error) throw new Error(error);
     
-    /* Find all the non-prefixes and send it*/
-    defaultOptions.url = code2040Url + 'prefix/validate';
-    body = JSON.parse(body);
-    console.log(body.prefix);
-    console.log(body.array);
-    defaultOptions.formData.array = body.array.filter(function(str) {
-      return !str.startsWith(body.prefix);
-    });
-    console.log(defaultOptions.formData);
-    request(defaultOptions, function (error, response, body) {
+    var url = code2040Url + 'prefix/validate';
+    var data = {
+      array: body.array.filter(function(str) {
+        return str.indexOf(body.prefix) != 0;
+      })
+    };
+    request(postOptions(url, data), function (error, response, body) {
       if (error) throw new Error(error);
       console.log(body);
     });
